a569a4865c532157a71b956b4746e653408fdb41
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-28 23:17:28 -0400

    Added favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,21 +3,23 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="nav-bar">
-      <Link to="/">
-        <img
-          src="/images/cherry_logo.png"
-          alt="cherry logo"
-          className="nav-logo"
-        />
-      </Link>
-      <Link to="/about" className="nav-link">
-        About
-      </Link>
-      <Link to="/projects" className="nav-link">
-        Projects
-      </Link>
-    </div>
+    <>
+      <div className="nav-bar">
+        <Link to="/">
+          <img
+            src="/images/cherry_logo.png"
+            alt="cherry logo"
+            className="nav-logo"
+          />
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+        <Link to="/projects" className="nav-link">
+          Projects
+        </Link>
+      </div>
+    </>
   );
 };
 
