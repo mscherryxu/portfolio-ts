e2cd509397cbb5d824762cd261e63bba04f3edc2
Cherry Xu <ms.cherry.xu@gmail.com>
2022-09-25 23:40:27 -0400

    Centered and added width to about page, edited projects for desktop view

--- src/components/About.js
+++ src/components/About.js
@@ -2,25 +2,27 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="parent-container">
-      <h1 className="intro">Hi, my name is Cherry Xu! 👋</h1>
-      <img src="/images/Cherry.jpeg" alt="Cherry" className="profile-image" />
-      <h2 className="career-title">
-        Registered Dietitian to an aspiring Full-Stack Software Engineer
-      </h2>
-      <h3>My Journey</h3>
-      <p>
-        As a Registered Dietitian since 2014, I've enjoyed working with others
-        and helping improve others' health through lifestyle changes and
-        nutrition education.
-      </p>
-      <p>
-        Now, I am looking for a change to constantly learn and grow and found
-        Software Engineering as my new passion. Time passes by so quickly with
-        building projects, creating applications, and using new technologies. I
-        would like to continue helping others with my newfound passion in
-        technology.
-      </p>
+    <div className="outer-container">
+      <div className="about-parent-container">
+        <h1 className="intro">Hi, my name is Cherry Xu! 👋</h1>
+        <img src="/images/Cherry.jpeg" alt="Cherry" className="profile-image" />
+        <h2 className="career-title">
+          Registered Dietitian to an aspiring Full-Stack Software Engineer
+        </h2>
+        <h3>My Journey</h3>
+        <p>
+          As a Registered Dietitian since 2014, I've enjoyed working with others
+          and helping improve others' health through lifestyle changes and
+          nutrition education.
+        </p>
+        <p>
+          Now, I am looking for a change to constantly learn and grow and found
+          Software Engineering as my new passion. Time passes by so quickly with
+          building projects, creating applications, and using new technologies.
+          I would like to continue helping others with my newfound passion in
+          technology.
+        </p>
+      </div>
     </div>
   );
 };
