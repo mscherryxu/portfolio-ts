7e50eb8db16cdc93f0d8dcaa1c90bd7b05aac6f2
Cherry Xu <ms.cherry.xu@gmail.com>
2022-09-28 15:54:16 -0400

    Added transition, links, and styling to hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  let menu;
-  let menuMask;
+  const maskTransitions =
+    useTransition(showMenu, {
+      from: { position: "absolute", opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 },
+    }) || [];
 
-  if (showMenu) {
-    menu = (
-      <div className="menu">
-        <div className="nav-links-bar-menu">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-          <Link to="/projects" className="nav-link">
-            Projects
-          </Link>
-          <a
-            href="https://docs.google.com/document/d/1KY6UrGO4Or2B-wIfGi2nHomGm5SX1UQVYEp5yImiB4U/edit?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
-            className="nav-link"
-          >
-            Resume
-          </a>
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
-        </div>
-      </div>
-    );
-    menuMask = (
-      <div className="menuMask" onClick={() => setShowMenu(false)}></div>
-    );
-  }
+  const menuTransitions =
+    useTransition(showMenu, {
+      from: { opacity: 0, transform: "translateX(-100%)" },
+      enter: { opacity: 1, transform: "translateX(0%)" },
+      leave: { opacity: 0, transform: "translateX(-100%)" },
+    }) || [];
 
   return (
     <>
@@ -53,8 +36,58 @@ const NavBar = () => {
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
-        {menuMask}
-        {menu}
+
+        {maskTransitions(
+          (props, item, key) =>
+            item && (
+              <animated.div
+                key={key}
+                style={props}
+                className="menuMask"
+                onClick={() => setShowMenu(false)}
+              ></animated.div>
+            )
+        )}
+
+        {menuTransitions(
+          (props, item, key) =>
+            item && (
+              <animated.div key={key} style={props} className="menu">
+                <div className="hamburger-container">
+                  <div className="nav-hamburger-logo">
+                    <Link to="/">
+                      <img
+                        src="/images/cherry_logo.png"
+                        alt="cherry logo"
+                        className="nav-logo"
+                      />
+                    </Link>
+                  </div>
+                  <div className="nav-links-bar-menu">
+                    <span className="navigate-to">Navigate to:</span>
+                    <Link to="/about" className="nav-link">
+                      About
+                    </Link>
+                    <Link to="/projects" className="nav-link">
+                      Projects
+                    </Link>
+                    <a
+                      href="https://docs.google.com/document/d/1KY6UrGO4Or2B-wIfGi2nHomGm5SX1UQVYEp5yImiB4U/edit?usp=sharing"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="nav-link"
+                    >
+                      Resume
+                    </a>
+                    <Link to="/contact" className="nav-link">
+                      Contact
+                    </Link>
+                  </div>
+                </div>
+              </animated.div>
+            )
+        )}
+
         <div className="nav-links">
           <Link to="/about" className="nav-link">
             About
