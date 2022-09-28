cfcab995ffa1bbe79c6e3d03212d33b18b2950e6
Cherry Xu <ms.cherry.xu@gmail.com>
2022-09-27 23:00:44 -0400

    Deleted video and script file

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navBarLinks = document.getElementsByClassName("nav-links")[0];
-
-toggleButton.addEventListener("click", () => {
-  navBarLinks.classList.toggle("active");
-});
