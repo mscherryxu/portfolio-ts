7e47931b1b731b0a1e5ada69f592cd8e55ec0232
Cherry Xu <ms.cherry.xu@gmail.com>
2022-09-30 13:27:06 -0400

    Deleted Home component and changed routes to About

--- src/RoutesComponent.js
+++ src/RoutesComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -9,7 +8,7 @@ const RoutesComponent = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
