1cb4b675d3dc0d5ab9e4863e4391d161e0e5a146
Cherry Xu <ms.cherry.xu@gmail.com>
2022-10-01 23:08:10 -0400

    Deleted unused code to deploy site changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { TextareaAutosize } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
