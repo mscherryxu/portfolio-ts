4deb469970f4fc5d69ba9563645d31a36cc7f539
Cherry Xu <ms.cherry.xu@gmail.com>
2022-10-01 22:32:19 -0400

    Added Home component to include picture and career change

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="outer-container">
+      <div className="about-parent-container">
+        <h1 className="intro">Hi, my name is Cherry Xu! 👋</h1>
+        <h2 className="career-title">
+          Registered Dietitian to an aspiring Full-Stack Software Engineer
+        </h2>
+        <img src="/images/Cherry.jpeg" alt="Cherry" className="profile-image" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
